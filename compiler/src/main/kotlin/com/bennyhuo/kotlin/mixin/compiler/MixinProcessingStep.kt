137dce0ac512f89e2d3ac4308633a1df1c9888d3
huobingqian <huobingqian@yuanfudao.com>
2022-01-02 10:34:08 +0800

    Default to generate mixins. Use 'mixin.library' to enable index generating for libraries.

--- compiler/src/main/kotlin/com/bennyhuo/kotlin/mixin/compiler/MixinProcessingStep.kt
+++ compiler/src/main/kotlin/com/bennyhuo/kotlin/mixin/compiler/MixinProcessingStep.kt
@@ -2,8 +2,6 @@ package com.bennyhuo.kotlin.mixin.compiler
 
 import androidx.room.compiler.processing.*
 import com.bennyhuo.kotlin.mixin.annotations.Mixin
-import com.squareup.javapoet.*
-import javax.tools.Diagnostic
 
 /**
  * Created by benny.
@@ -17,15 +15,15 @@ class MixinProcessingStep : XProcessingStep {
         env: XProcessingEnv,
         elementsByAnnotation: Map<String, Set<XElement>>
     ): Set<XElement> {
-        val isMainModule = env.options["mixin.main"]?.toBoolean() ?: false
+        val isLibraryModule = env.options["mixin.library"]?.toBoolean() ?: false
         val elements = elementsByAnnotation[Mixin::class.java.name]
             ?.filterIsInstance<XTypeElement>()
             ?: return emptySet()
-        
-        if (isMainModule) {
-            MixinGenerator().generate(env, elements + MixinIndexLoader().loadIndex(env))
-        } else {
+
+        if (isLibraryModule) {
             MixinIndexGenerator().generate(env, elements)
+        } else {
+            MixinGenerator().generate(env, elements + MixinIndexLoader().loadIndex(env))
         }
         return emptySet()
     }
